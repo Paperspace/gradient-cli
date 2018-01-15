736f663d55778ad37d89bdbcc882163d7c813e82
Tom Sanfilippo <sanfilip@gmail.com>
2018-01-15 18:07:48 -0500

    fix sample project name

--- remote_test.py
+++ remote_test.py
@@ -3,7 +3,7 @@ import paperspace
 
 paperspace.config.PAPERSPACE_API_KEY = '14a4bc1cbc414...'
 
-paperspace.jobs.runas_job({'project': 'myprojec', 'machineType': 'GPU+', 'container': 'Test-Container'})
+paperspace.jobs.runas_job({'project': 'myproject', 'machineType': 'GPU+', 'container': 'Test-Container'})
 
 print(os.getcwd())
 print('something useful')
