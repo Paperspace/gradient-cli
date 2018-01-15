2efd2fe07fe26e249f94dcc9eac805777bf76648
Tom Sanfilippo <sanfilip@gmail.com>
2018-01-15 18:09:02 -0500

    changes for publishing to pypi

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-# paperspace-python
-Paperspace API for python
+Paperspace API for Python
+=========================
 
-Coming soon...
+Sample usage:
+
+    import paperspace
+
+    paperspace.config.PAPERSPACE_API_KEY = '14a4bc1cbc414...'
+
+    paperspace.jobs.runas_job({'project': 'myproject', 'machineType': 'GPU+', 'container': 'Test-Container'})
+
+    print('hello paperspace!')
+
+
+Contributing
+============
 
 Want to contribute?  Contact us at hello@paperspace.com
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='paperspace',
+    version='0.0.2',
+    description='Paperspace API for Python',
+    long_description=long_description,
+    url='https://github.com/paperspace/paperspace-python',
+    author='Paperspace Co.',
+    author_email='info@paperspace.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='paperspace api development library',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['boto3', 'botocore', 'requests'],
+)
