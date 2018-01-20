4d5f174f0de22dbf3c4523bb84467b798adc875f
Tom Sanfilippo <sanfilip@gmail.com>
2018-01-20 01:57:55 -0500

    rename remote_test.py to test_remote.py

--- test_remote.py
+++ test_remote.py
@@ -1,8 +1,6 @@
 import os
 import paperspace
 
-paperspace.config.PAPERSPACE_API_KEY = '14a4bc1cbc414...'
-
 paperspace.jobs.runas_job({'project': 'myproject', 'machineType': 'GPU+', 'container': 'Test-Container'})
 
 print(os.getcwd())
