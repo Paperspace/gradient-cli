39ee04f7fd6ce8e2f4152a619162a1bcada1d199
Tom Sanfilippo <sanfilip@gmail.com>
2018-01-20 01:24:05 -0500

    allow env overrides for CONFIG_HOST and CONFIG_LOG_HOST

--- paperspace/config.py
+++ paperspace/config.py
@@ -8,3 +8,7 @@ config.CONFIG_LOG_HOST = 'https://logs.paperspace.io'
 
 if 'PAPERSPACE_API_KEY' in os.environ:
     config.PAPERSPACE_API_KEY = os.environ['PAPERSPACE_API_KEY']
+if 'PAPERSPACE_CONFIG_HOST' in os.environ:
+    config.CONFIG_HOST = os.environ['PAPERSPACE_CONFIG_HOST']
+if 'PAPERSPACE_CONFIG_LOG_HOST' in os.environ:
+    config.CONFIG_LOG_HOST = os.environ['PAPERSPACE_CONFIG_LOG_HOST']
