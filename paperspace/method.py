40a4068ac1c2a955cd843c1eddd55ae046bc2bba
Dominik Andruszak <dandruszak@gmail.com>
2019-02-12 18:38:04 +0100

    Quick fix for importing

--- paperspace/method.py
+++ paperspace/method.py
@@ -10,7 +10,6 @@ import requests
 import subprocess
 
 from .config import *
-from .__init__ import __version__
 
 def zip_to_tmp(files, ignore_files=[]):
     file = files[0]
@@ -137,7 +136,7 @@ def method(category, method, params):
             params = None
         r = requests.request(http_method, config.CONFIG_HOST + path,
                              headers={'x-api-key': config.PAPERSPACE_API_KEY, 'ps_client_name': 'paperspace-python', 
-                             'ps_client_version' : __version__},
+                             'ps_client_version' : "0.0.15"},
                              params=params, data=data, files=files)
         #pprint(vars(r.request))
     except requests.exceptions.RequestException as e:
