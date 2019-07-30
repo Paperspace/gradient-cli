07310d0257c57e32aeb220ddd714cc8a28c5f540
SebastianMeler <sebastianissimus@gmail.com>
2019-07-30 09:24:44 +0200

    Update gradient/api_sdk/clients/base_client.py
    Co-Authored-By: Jared Scheib <jaredscheib@users.noreply.github.com>

--- gradient/api_sdk/clients/base_client.py
+++ gradient/api_sdk/clients/base_client.py
@@ -14,7 +14,7 @@ class BaseClient(object):
         Base class. All client classes inherit from it.
 
         An API key can be created at paperspace.com after you sign in to your account. After obtaining it, you can set
-        it in CLI using command::
+        it in the CLI using the command::
             ``gradient apiKey XXXXXXXXXXXXXXXXXXX``
         or you can include your API key for each command ,for example:: ``gradient experiments run ... --apiKey XXXXXXXXXXXXXXXXXXX``
 
