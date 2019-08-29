c9cc306797bc1e6ace065f0b6e5d415b6cde1ca2
BartoszCki <bartosz.cierocki@gmail.com>
2019-08-29 15:03:50 +0200

    Rename file for consistency

--- gradient/api_sdk/clients/__init__.py
+++ gradient/api_sdk/clients/__init__.py
@@ -2,7 +2,7 @@ from .deployment_client import DeploymentsClient
 from .experiment_client import ExperimentsClient
 from .hyperparameter_client import HyperparameterJobsClient
 from .job_client import JobsClient
-from .machines import MachinesClient
+from .machines_client import MachinesClient
 from .model_client import ModelsClient
 from .project_client import ProjectsClient
 from .sdk_client import SdkClient
