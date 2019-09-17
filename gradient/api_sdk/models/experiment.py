5b4dd6bfdee706965c7889a535df7c16494a7b51
Britney Luckey <bluckey@paperspace.com>
2019-09-17 18:18:02 -0400

    Add support for experiment workspace creds (PS-11088)

--- gradient/api_sdk/models/experiment.py
+++ gradient/api_sdk/models/experiment.py
@@ -16,6 +16,8 @@ class BaseExperiment(object):
     :param str name: Name of new experiment
     :param str ports: Port to use in new experiment
     :param str workspace_url: Project git repository url
+    :param str workspace_username: Project git repository username
+    :param str workspace_password: Project git repository password
     :param str working_directory: Working directory for the experiment
     :param str artifact_directory: Artifacts directory
     :param str cluster_id: Cluster ID (handle)
@@ -44,6 +46,8 @@ class BaseExperiment(object):
     name = attr.ib(type=str, default=None)
     ports = attr.ib(type=str, default=None)
     workspace_url = attr.ib(type=str, default=None)
+    workspace_username = attr.ib(type=str, default=None)
+    workspace_password = attr.ib(type=str, default=None)
     working_directory = attr.ib(type=str, default=None)
     artifact_directory = attr.ib(type=str, default=None)
     cluster_id = attr.ib(type=str, default=None)
