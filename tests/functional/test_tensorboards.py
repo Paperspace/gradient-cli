656791dd78a91dd9a9446de2584b3a90c622dcbd
Paweł Mudlaff <pawel@paperspace.com>
2019-09-20 13:20:41 +0200

    Fix test for sending add experiments with wrong api key

--- tests/functional/test_tensorboards.py
+++ tests/functional/test_tensorboards.py
@@ -315,7 +315,7 @@ class TestTensorboardsAddExperiment(object):
     EXPECTED_HEADERS_WITH_CHANGED_API_KEY["X-API-Key"] = "some_key"
 
     RESPONSE_JSON_WITH_WRONG_API_TOKEN = {"title": "Invalid credentials provided"}
-    EXPECTED_STDOUT_WITH_WRONG_API_TOKEN = "Failed to fetch data\n"
+    EXPECTED_STDOUT_WITH_WRONG_API_TOKEN = "Failed to fetch data: Invalid credentials provided\n"
 
     @mock.patch("gradient.api_sdk.clients.http_client.requests.post")
     def test_should_send_valid_request_when_command_was_executed_with_required_options(self, post_patched):
@@ -334,17 +334,18 @@ class TestTensorboardsAddExperiment(object):
                                              )
         assert self.EXPECTED_HEADERS["X-API-Key"] != "some_key"
 
-    # TODO after fix in API to return correct response status code when credentials are incorrect
-    # @mock.patch("gradient.api_sdk.clients.http_client.requests.post")
-    # def test_should_send_request_with_changed_api_key_when_api_key_option_was_used(self, post_patched):
-    #     post_patched.return_value = MockResponse(self.RESPONSE_JSON_WITH_WRONG_API_TOKEN)
-    #
-    #     runner = CliRunner()
-    #     result = runner.invoke(cli.cli, self.COMMAND_WITH_API_KEY_CHANGED)
-    #
-    #     assert result.output == self.EXPECTED_STDOUT_WITH_WRONG_API_TOKEN, result.exc_info
-    #     post_patched.assert_called_once_with(self.URL,
-    #                                          headers=self.EXPECTED_HEADERS_WITH_CHANGED_API_KEY,
-    #                                          json=self.EXPECTED_REQUEST_JSON,
-    #                                          params=None)
-    #     assert self.EXPECTED_HEADERS["X-API-Key"] != "some_key"
+    @mock.patch("gradient.api_sdk.clients.http_client.requests.post")
+    def test_should_send_request_with_changed_api_key_when_api_key_option_was_used(self, post_patched):
+        post_patched.return_value = MockResponse(self.RESPONSE_JSON_WITH_WRONG_API_TOKEN, status_code=401)
+
+        runner = CliRunner()
+        result = runner.invoke(cli.cli, self.COMMAND_WITH_API_KEY_CHANGED)
+
+        assert result.output == self.EXPECTED_STDOUT_WITH_WRONG_API_TOKEN, result.exc_info
+        post_patched.assert_called_once_with(self.URL,
+                                             headers=self.EXPECTED_HEADERS_WITH_CHANGED_API_KEY,
+                                             json=self.EXPECTED_REQUEST_JSON,
+                                             params=None,
+                                             data=None,
+                                             files=None
+                                             )
