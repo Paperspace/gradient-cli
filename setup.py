07f8b89b51e5e0b3ee3137e3d567266b958e62d3
Michał Kułaczkowski <m.kulaczkowski@gmail.com>
2020-01-08 15:05:56 +0100

    Updated tox.ini

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ setup(
         'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
