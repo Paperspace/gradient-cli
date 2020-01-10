01ef3c03034cc6e9c0fb9275d0358894f25eb52a
BartoszCki <bartosz.cierocki@gmail.com>
2020-01-10 14:28:46 +0100

    Stop sending null values in datasets

--- gradient/api_sdk/serializers/dataset.py
+++ gradient/api_sdk/serializers/dataset.py
@@ -1,7 +1,9 @@
 import marshmallow as ma
 
+from .base import BaseSchema
 
-class DatasetSchema(ma.Schema):
+
+class DatasetSchema(BaseSchema):
     uri = ma.fields.String(required=True)
     aws_access_key_id = ma.fields.String(dump_to="awsAccessKeyId", load_from="awsAccessKeyId")
     aws_secret_access_key = ma.fields.String(dump_to="awsSecretAccessKey", load_from="awsSecretAccessKey")
