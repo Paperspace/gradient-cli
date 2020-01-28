d85e653b44a7507dccc4e053cdf98404b6e22643
Paweł Mudlaff <pawel@paperspace.com>
2020-01-28 15:59:16 +0100

    Add remove_tags and list_tags methods to tag client

--- gradient/api_sdk/clients/tag_client.py
+++ gradient/api_sdk/clients/tag_client.py
@@ -35,3 +35,35 @@ class TagClient(BaseClient):
 
         update_tag_repository = UpdateTagRepository(api_key=self.api_key, logger=self.logger)
         update_tag_repository.update(entity=entity, entity_id=entity_id, tags=tags)
+
+    def remove_tags(self, entity_id, entity, tags):
+        """
+        Remove tags from entity.
+        :param str entity_id:
+        :param str entity:
+        :param list[str] tags: list of tags to remove from entity
+        :return:
+        """
+        self._validate_entities(entity)
+
+        list_tag_repository = ListTagRepository(api_key=self.api_key, logger=self.logger)
+        entity_tags = list_tag_repository.list(entity=entity, entity_ids=[entity_id])
+
+        if entity_tags:
+            entity_tags = set(entity_tags) - set(tags)
+
+            update_tag_repository = UpdateTagRepository(api_key=self.api_key, logger=self.logger)
+            update_tag_repository.update(entity=entity, entity_id=entity_id, tags=entity_tags)
+
+    def list_tags(self, entity_ids, entity):
+        """
+        List tags for entity
+        :param list[str] entity_ids:
+        :param str entity:
+        :return:
+        """
+        self._validate_entities(entity)
+
+        list_tag_repository = ListTagRepository(api_key=self.api_key, logger=self.logger)
+        entity_tags = list_tag_repository.list(entity=entity, entity_ids=entity_ids)
+        return entity_tags
